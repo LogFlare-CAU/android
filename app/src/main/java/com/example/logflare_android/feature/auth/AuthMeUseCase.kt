5f1a29c30d9f8fa2c86f70c5db99e435af058704
Code0987 <macqueen0987@gmail.com>
2025-12-05 22:12:22 +0900

    fcm 설정 동기화 개선
    푸시 알림 전송에 실패하면 백엔드단에서 토큰 삭제해버리기 때문에 프론트 단에서 앱 켜면 이를 재 동기화

--- app/src/main/java/com/example/logflare_android/feature/auth/AuthMeUseCase.kt
+++ app/src/main/java/com/example/logflare_android/feature/auth/AuthMeUseCase.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.example.logflare.core.model.UserDTO
 import com.example.logflare.core.network.LogflareApi
 import com.example.logflare_android.data.AuthRepository
+import com.example.logflare_android.data.DeviceRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,6 +12,7 @@ import javax.inject.Singleton
 class AuthMeUseCase @Inject constructor(
     private val api: LogflareApi,
     private val authRepository: AuthRepository,
+    private val deviceRepository: DeviceRepository
 ){
     suspend operator fun invoke(): UserDTO?{
         val token = authRepository.getToken()
@@ -20,6 +22,7 @@ class AuthMeUseCase @Inject constructor(
             return null
             // TODO: 실제 예외 처리
         }
+        deviceRepository.syncConfigAndRegister()
         result.data?.username?.let { authRepository.setUsername(it) }
         return result.data
     }
